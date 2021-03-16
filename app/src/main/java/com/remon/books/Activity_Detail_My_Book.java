de4b0ed9344e556d51e8b6c35ab14576318da598
superphs333 <54734702+superphs333@users.noreply.github.com>
2021-03-16 10:56:46 +0900

    my_book - 삭제 ing

--- app/src/main/java/com/remon/books/Activity_Detail_My_Book.java
+++ app/src/main/java/com/remon/books/Activity_Detail_My_Book.java
@@ -3,12 +3,24 @@ package com.remon.books;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Activity_Detail_My_Book extends AppCompatActivity {
+
+    /*
+    뷰변수
+     */
+    TextView txt_title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__detail__my__book);
+
+        /*
+        뷰연결
+         */
+        txt_title = findViewById(R.id.txt_title);
+
     }
 }
--- app/src/main/java/com/remon/books/Adapter/Adater_My_Book.java
+++ app/src/main/java/com/remon/books/Adapter/Adater_My_Book.java
@@ -1,6 +1,10 @@
 package com.remon.books.Adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +15,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.remon.books.Activity_Detail_My_Book;
 import com.remon.books.Data.Data_My_Book;
 import com.remon.books.R;
 
@@ -37,7 +42,7 @@ public class Adater_My_Book
 
         // 위젯정의
         protected ImageView img_thumbnail;
-        protected TextView txt_title, txt_authors, txt_contents,txt_rating,txt_status;
+        protected TextView txt_title, txt_authors, txt_contents,txt_rating,txt_status,txt_function;
 
 
         public CustomViewHolder(@NonNull View itemView) {
@@ -49,6 +54,7 @@ public class Adater_My_Book
             this.txt_contents = (TextView)itemView.findViewById(R.id.txt_contents);
             this.txt_rating = (TextView)itemView.findViewById(R.id.txt_rating);
             this.txt_status = (TextView)itemView.findViewById(R.id.txt_status);
+            this.txt_function = (TextView)itemView.findViewById(R.id.txt_function);
 
         }
     }
@@ -67,7 +73,7 @@ public class Adater_My_Book
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final CustomViewHolder holder, int position) {
         // 이미지 셋팅
         Glide.with(holder.itemView.getContext()).load(arrayList.get(holder.getAdapterPosition()).getThumbnail()).into(holder.img_thumbnail);
         // 타이틀
@@ -89,6 +95,44 @@ public class Adater_My_Book
         }else{ // 읽음
             holder.txt_status.setText(context.getString(R.string.read_end));
         }
+
+        // 클릭시 -> Activity_Detail_My_Book 이동
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 페이지 이동
+                Intent intent = new Intent(context, Activity_Detail_My_Book.class);
+                intent.putExtra("unique_book_value",arrayList.get(holder.getAdapterPosition()).getUnique_book_value());
+                context.startActivity(intent);
+            }
+        });
+
+        // txt_function(점세개) 클릭 -> 수정 or 삭제
+        holder.txt_function.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder
+                        = new AlertDialog.Builder(context);
+
+                // 만약 from_=search라면 => 수정이 보이지 않아야 함
+                if(arrayList.get(holder.getAdapterPosition()).getFrom_().equals(""))
+                final String str[] = {"A","B"};
+                builder.setTitle("선택하세요")
+                        .setNegativeButton("취소",null)
+                        .setItems(str,// 리스트 목록에 사용할 배열
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Log.d("실행","선택된것="+str[which]);
+
+                                    }
+                                }
+                        );
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            }
+        }); // end holder.txt_function.setOnClickListener
 
     }
 
