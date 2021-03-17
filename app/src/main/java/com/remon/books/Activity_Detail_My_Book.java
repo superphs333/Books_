4ce6a8215d1feed5da1fef4339f67458715f65f0
superphs333 <54734702+superphs333@users.noreply.github.com>
2021-03-18 08:04:15 +0900

    my_book - 자세히보기 ing - 서평쓰기 end

--- app/src/main/java/com/remon/books/Activity_Detail_My_Book.java
+++ app/src/main/java/com/remon/books/Activity_Detail_My_Book.java
@@ -8,16 +8,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.bumptech.glide.Glide;
 import com.remon.books.Data.Data_My_Book;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -78,6 +85,65 @@ public class Activity_Detail_My_Book extends AppCompatActivity {
         Log.d("실행", "unique_book_value="+unique_book_value);
 
 
+        // 카테고리 변경시 -> 데이터베이스 반영
+        category_read_status.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // 데이터베이스에 반영
+                // 웹페이지 실행하기
+                String url = getString(R.string.server_url)+"웹페이지주소";
+
+                StringRequest request = new StringRequest(
+                        Request.Method.POST,
+                        url,
+                        new com.android.volley.Response.Listener<String>() { // 정상 응답
+                            @Override
+                            public void onResponse(String response) {
+                                Log.d("실행","response=>"+response);
+
+                            }
+                        },
+                        new com.android.volley.Response.ErrorListener() { // 에러 발생
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d("실행","error=>"+error.getMessage());
+                            }
+                        }
+
+                ){ // Post 방식으로 body에 요청 파라미터를 넣어 전달하고 싶을 경우
+                    // 만약 헤더를 한 줄 추가하고 싶다면 getHeaders() override
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("idx", "post로 넣을 값");
+                        return params;
+                    }
+                };
+
+                // 요청 객체를 만들었으니 이제 requestQueue 객체에 추가하면 됨.
+                // Volley는 이전 결과를 캐싱하므로, 같은 결과가 있으면 그대로 보여줌
+                // 하지만 아래 메소드를 false로 set하면 이전 결과가 있더라도 새로 요청해서 응답을 보여줌.
+                request.setShouldCache(false);
+                AppHelper.requestQueue.add(request);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
         /*
         unique_book_value에 해당하는 데이터 불러오기
@@ -119,17 +185,15 @@ public class Activity_Detail_My_Book extends AppCompatActivity {
 
             }
         });
+    } // end onResume
 
 
-
-
-
-
-    }
 
     // 액티비티 Activity_Review_Write로 이동
     public void go_to_Activity_Review_Write(View view) {
         Intent intent = new Intent(context,Activity_Review_Write.class);
+        intent.putExtra(getString(R.string.unique_book_value),unique_book_value);
+        intent.putExtra("review",txt_review.getText().toString());
         startActivity(intent);
     }
 
--- app/src/main/java/com/remon/books/Activity_Review_Write.java
+++ app/src/main/java/com/remon/books/Activity_Review_Write.java
@@ -1,14 +1,110 @@
 package com.remon.books;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.remon.books.Function.Function_SharedPreference;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Activity_Review_Write extends AppCompatActivity {
+
+    EditText edit_review;
+    Button btn_review_write;
+
+    Function_SharedPreference fshared;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__review__write);
+
+        /*
+        뷰연결
+         */
+        edit_review = findViewById(R.id.edit_review);
+        btn_review_write = findViewById(R.id.btn_review_write);
+
+        // 리뷰셋팅
+        edit_review.setText(getIntent().getStringExtra("review"));
+
+        /*
+        함수
+         */
+        fshared = new Function_SharedPreference(getApplicationContext());
+
+
+    }
+
+    public void Review_Send(View view) {
+
+        // 내용이 공백인지 확인
+        final String review = edit_review.getText().toString().trim();
+        if(review.equals("")){
+            Toast.makeText(getApplicationContext(), "리뷰를 입력해주세요",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // 웹페이지 실행하기
+        String url = getString(R.string.server_url)+"Book_Review_Add.php";
+
+        StringRequest request = new StringRequest(
+                Request.Method.POST,
+                url,
+                new com.android.volley.Response.Listener<String>() { // 정상 응답
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("실행","response=>"+response);
+
+                        if(response.equals("success")){
+                            Toast.makeText(getApplicationContext(), "리뷰가 등록되었습니다",Toast.LENGTH_LONG).show();
+                            finish();
+                        }else{
+                            Toast.makeText(getApplicationContext(), "문제가 발생하였습니다. 다시 시도해 주세요",Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                },
+                new com.android.volley.Response.ErrorListener() { // 에러 발생
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("실행","error=>"+error.getMessage());
+                        Toast.makeText(getApplicationContext(), "문제가 발생하였습니다. 다시 시도해주세요",Toast.LENGTH_LONG).show();
+
+                    }
+                }
+
+        ){ // Post 방식으로 body에 요청 파라미터를 넣어 전달하고 싶을 경우
+            // 만약 헤더를 한 줄 추가하고 싶다면 getHeaders() override
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                // 보낼값 : login_value, unique_book_value, review
+                params.put(getString(R.string.login_value), fshared.getPreferenceString(getString(R.string.member),getString(R.string.login_value)));
+                params.put(getString(R.string.unique_book_value), getIntent().getStringExtra(getString(R.string.unique_book_value)));
+                params.put("review",edit_review.getText().toString());
+
+
+                return params;
+            }
+        };
+
+        // 요청 객체를 만들었으니 이제 requestQueue 객체에 추가하면 됨.
+        // Volley는 이전 결과를 캐싱하므로, 같은 결과가 있으면 그대로 보여줌
+        // 하지만 아래 메소드를 false로 set하면 이전 결과가 있더라도 새로 요청해서 응답을 보여줌.
+        request.setShouldCache(false);
+        AppHelper.requestQueue.add(request);
+
     }
 }
