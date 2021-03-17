5b94017ae7820c341aec33b8cbf16d9510f521f4
superphs333 <54734702+superphs333@users.noreply.github.com>
2021-03-17 10:53:33 +0900

    my_book - 자세히보기 ing

--- app/src/main/java/com/remon/books/Activity_Detail_My_Book.java
+++ app/src/main/java/com/remon/books/Activity_Detail_My_Book.java
@@ -3,6 +3,7 @@ package com.remon.books;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class Activity_Detail_My_Book extends AppCompatActivity {
@@ -12,15 +13,28 @@ public class Activity_Detail_My_Book extends AppCompatActivity {
      */
     TextView txt_title;
 
+    // unique_book_value
+    String unique_book_value;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__detail__my__book);
 
         /*
+        데이터 불러오기
+         */
+        unique_book_value = getIntent().getStringExtra("unique_book_value");
+        Log.d("실행", "unique_book_value="+unique_book_value);
+
+
+
+        /*
         뷰연결
          */
         txt_title = findViewById(R.id.txt_title);
+
+
 
     }
 }
