0f95fb6c7c4a57921f6f79256bc7dce1b6dfad0e
superphs333 <54734702+superphs333@users.noreply.github.com>
2021-04-06 07:31:14 +0900

    댓글 ING2

--- app/src/main/java/com/remon/books/Activity_Add_Comment.java
+++ app/src/main/java/com/remon/books/Activity_Add_Comment.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -15,10 +16,14 @@ import com.remon.books.Function.Function_SharedPreference;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Activity_Add_Comment extends AppCompatActivity {
+public class Activity_Add_Comment extends AppCompatActivity implements View.OnClickListener{
 
     // 메모 idx
     int idx_memo;
+
+
+    // 뷰
+    EditText edit_comment;
 
     // 함수
     Function_SharedPreference fshared;
@@ -29,6 +34,8 @@ public class Activity_Add_Comment extends AppCompatActivity {
         setContentView(R.layout.activity__add__comment);
 
         // 뷰연결
+        edit_comment = findViewById(R.id.edit_comment);
+
 
         // 함숫셋팅
         fshared = new Function_SharedPreference(getApplicationContext());
@@ -39,8 +46,9 @@ public class Activity_Add_Comment extends AppCompatActivity {
 
     }
 
-    public void onClick(View view){
-        int id = view.getId();
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
 
         // 뒤로가기
         if(id==R.id.img_back){
@@ -49,44 +57,56 @@ public class Activity_Add_Comment extends AppCompatActivity {
 
         // 댓글 보내기
         if(id==R.id.btn_comment){
-            // 웹페이지 실행하기
-            String url = getString(R.string.server_url)+"Management_Comment.php";
-
-            StringRequest request = new StringRequest(
-                    Request.Method.POST,
-                    url,
-                    new com.android.volley.Response.Listener<String>() { // 정상 응답
-                        @Override
-                        public void onResponse(String response) {
-                            Log.d("실행","response=>"+response);
-
-                        }
-                    },
-                    new com.android.volley.Response.ErrorListener() { // 에러 발생
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Log.d("실행","error=>"+error.getMessage());
-                        }
-                    }
-
-            ){ // Post 방식으로 body에 요청 파라미터를 넣어 전달하고 싶을 경우
-                // 만약 헤더를 한 줄 추가하고 싶다면 getHeaders() override
-                @Override
-                protected Map<String, String> getParams() throws AuthFailureError {
-                    Map<String, String> params = new HashMap<String, String>();
-                    params.put("login_value", fshared.get_login_value());
-                    return params;
-                }
-            };
-
-            // 요청 객체를 만들었으니 이제 requestQueue 객체에 추가하면 됨.
-            // Volley는 이전 결과를 캐싱하므로, 같은 결과가 있으면 그대로 보여줌
-            // 하지만 아래 메소드를 false로 set하면 이전 결과가 있더라도 새로 요청해서 응답을 보여줌.
-            request.setShouldCache(false);
-            AppHelper.requestQueue.add(request);
+            Management_Comment("add");
 
         }
+    }
 
+    // 댓글을 추가, 수정, 삭제하는 함수
+    private void Management_Comment(final String sort){
+        // sort = add,add_comment,edit, delete
 
-    } // end onClick
+        // 채팅내용
+        final String comment = edit_comment.getText().toString();
+
+        // 웹페이지 실행하기
+        String url = getString(R.string.server_url)+"Management_Comment.php";
+
+        StringRequest request = new StringRequest(
+                Request.Method.POST,
+                url,
+                new com.android.volley.Response.Listener<String>() { // 정상 응답
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("실행","response=>"+response);
+
+                    }
+                },
+                new com.android.volley.Response.ErrorListener() { // 에러 발생
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("실행","error=>"+error.getMessage());
+                    }
+                }
+
+        ){ // Post 방식으로 body에 요청 파라미터를 넣어 전달하고 싶을 경우
+            // 만약 헤더를 한 줄 추가하고 싶다면 getHeaders() override
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("sort", sort);
+                params.put("login_value", fshared.get_login_value());
+                params.put("idx_memo", String.valueOf(idx_memo));
+                params.put("comment",comment);
+
+                return params;
+            }
+        };
+
+        // 요청 객체를 만들었으니 이제 requestQueue 객체에 추가하면 됨.
+        // Volley는 이전 결과를 캐싱하므로, 같은 결과가 있으면 그대로 보여줌
+        // 하지만 아래 메소드를 false로 set하면 이전 결과가 있더라도 새로 요청해서 응답을 보여줌.
+        request.setShouldCache(false);
+        AppHelper.requestQueue.add(request);
+    } // end Management_Comment()
 }
